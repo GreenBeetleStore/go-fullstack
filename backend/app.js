3cb8e86f90c144d0dfff2e02bb9da961d207ddcb
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-10-01 15:35:58 +0200

    app.js i instalar Express

---
+++ backend/app.js
@@ -0,0 +1,6 @@
+// Importar express.
+const express = require('express');
+// Crear la constant app(la nostra aplicació) buida cridant el métode express.
+const app = express();
+// Exportem aquesta aplicació.
+module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
-const http = require('http');
-
+// Importar el package (http) de node.
+const http = require("http");
+// Crear el nostre server, cridant al métode (createServer) que conté els arguments consulta i resposta(requête i réponse).
 const server = http.createServer((req, res) => {
-      res.end('Voilà la première réponse du server !');
+      // De moment només enviem una sola resposta.Cridar el métode (end).
+  res.end("Voilà la réponse du serveur !");
 });
-
+// El server està a l'escolta de la consulta(requête) enviada, amb el métode listen del server al port 3000.
 server.listen(process.env.PORT || 3000);
+ // En cas que el port 3000 no està disponible utilitzem una variable entorn(environement) per utilitzar el port que ens envia l'entorn.
