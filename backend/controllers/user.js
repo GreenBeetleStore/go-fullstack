4d905ccccb8d8ba44270d71935cbf78fb885f522
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-10-03 00:09:09 +0200

    TOKENS

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,7 @@
 // in controllers/user.js
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const User = require("../models/User");
 
 exports.signup = (req, res, next) => {
@@ -36,7 +38,11 @@ exports.login = (req, res, next) => {
           }
           res.status(200).json({
             userId: user._id,
-            token: "TOKEN",
+            token: jwt.sign(
+              { userId: user._id },
+              "RANDOM_TOKEN_SECRET", 
+              { expiresIn: "24h" }
+            )
           });
         })
         .catch((error) => res.status(500).json({ error }));
