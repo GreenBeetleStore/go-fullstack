1ca6799b4b4a07d45489b85adddacf92d32713fc
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-10-03 00:56:19 +0200

    Install & config multer

---
+++ backend/middleware/multer-config.js
@@ -0,0 +1,32 @@
+// in middleware/multer-config.js
+const multer = require("multer");
+
+const MIME_TYPES = {
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "images");
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(" ").join("_");
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + "." + extension);
+  },
+});
+
+module.exports = multer({ storage: storage }).single("image");
+
+
+/** Dans ce middleware :
+
+1. Nous créons une constante storage , à passer à multer comme configuration, qui contient la logique nécessaire pour indiquer à multer où enregistrer les fichiers entrants :
+
+      · la fonction destination indique à multer d'enregistrer les fichiers dans le dossier images ;
+
+      · la fonction filename indique à multer d'utiliser le nom d'origine, de remplacer les espaces par des underscores et d'ajouter un timestamp Date.now() comme nom de fichier. Elle utilise ensuite la constante dictionnaire de type MIME pour résoudre l'extension de fichier appropriée.
+
+2. Nous exportons ensuite l'élément multer entièrement configuré, lui passons notre constante storage et lui indiquons que nous gérerons uniquement les téléchargements de fichiers image. */
