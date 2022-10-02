531dade423bb8ab6a57ef1a48a3d87e76ddb4309
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-10-02 08:21:17 +0200

    models / thing.js

---
+++ backend/models/thing.js
@@ -0,0 +1,17 @@
+/** Ici, voici ce que nous faisons :
+
+· nous créons un schéma de données qui contient les champs souhaités pour chaque Thing, indique leur type ainsi que leur caractère (obligatoire ou non). Pour cela, on utilise la méthode Schema mise à disposition par Mongoose. Pas besoin de mettre un champ pour l'Id puisqu'il est automatiquement généré par Mongoose ;
+
+· ensuite, nous exportons ce schéma en tant que modèle Mongoose appelé « Thing », le rendant par là même disponible pour notre application Express. */
+
+const mongoose = require('mongoose');
+
+const thingSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  userId: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Thing', thingSchema);
