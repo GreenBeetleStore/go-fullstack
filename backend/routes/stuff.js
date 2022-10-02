098d269669a19f14a481f15212d403e75d492f9f
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-10-02 22:00:50 +0200

    format

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -4,10 +4,10 @@ const router = express.Router();
 
 const stuffCtrl = require("../controllers/stuff");
 
+router.get("/", stuffCtrl.getAllThings);
 router.post("/", stuffCtrl.createThing);
+router.get("/:id", stuffCtrl.getOneThing);
 router.put("/:id", stuffCtrl.modifyThing);
 router.delete("/:id", stuffCtrl.deleteThing);
-router.get("/:id", stuffCtrl.getOneThing);
-router.get("/", stuffCtrl.getAllThings);
 
 module.exports = router;
