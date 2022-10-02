5887021ff013fd43ed669266d6c35bcb3a51f0ca
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-10-02 21:03:47 +0200

    Començar capítol 13

--- Capítols/Capitol_11/app.js
+++ Capítols/Capitol_11/app.js
@@ -17,6 +17,8 @@ app.use((req, res, next) => {
   next();
 });
 
+/**  Chapitre 10. Enregistrement des Things dans la base de données.
+ *  Ici, vous créez une instance de votre modèle Thing en lui passant un objet JavaScript contenant toutes les informations requises du corps de requête analysé (en ayant supprimé en amont le faux_id envoyé par le front-end). */
 app.post("/api/stuff", (req, res, next) => {
   delete req.body._id;
   const thing = new Thing({
@@ -28,24 +30,39 @@ app.post("/api/stuff", (req, res, next) => {
     .catch((error) => res.status(400).json({ error }));
 });
 
+/** Chapitre 10. Récupération d'un Thing spécifique.
+ * Dans cette route :
+  · nous utilisons la méthode get() pour répondre uniquement aux demandes GET à cet endpoint ;
+  · nous utilisons deux-points : en face du segment dynamique de la route pour la rendre accessible en tant que paramètre ;
+  · nous utilisons ensuite la méthode findOne() dans notre modèle Thing pour trouver le Thing unique ayant le même _id que le paramètre de la requête ;
+  · ce Thing est ensuite retourné dans une Promise et envoyé au front-end ;
+  · si aucun Thing n'est trouvé ou si une erreur se produit, nous envoyons une erreur 404 au front-end, avec l'erreur générée.
+ */
 app.get("/api/stuff/:id", (req, res, next) => {
   Thing.findOne({ _id: req.params.id })
     .then((thing) => res.status(200).json(thing))
     .catch((error) => res.status(404).json({ error }));
 });
 
+/** Chapitre 11. Mettez à jour un Thing existant.
+ * Ci-dessus, nous exploitons la méthode updateOne() dans notre modèle Thing . Cela nous permet de mettre à jour le Thing qui correspond à l'objet que nous passons comme premier argument. Nous utilisons aussi le paramètre id passé dans la demande, et le remplaçons par le Thing passé comme second argument.
+ */
 app.put("/api/stuff/:id", (req, res, next) => {
   Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
     .then(() => res.status(200).json({ message: "Objet modifié !" }))
     .catch((error) => res.status(400).json({ error }));
 });
 
+/** Chapitre 11. Suppression d'un Thing.
+ * La méthode deleteOne() de notre modèle fonctionne comme findOne() et updateOne() dans le sens où nous lui passons un objet correspondant au document à supprimer. Nous envoyons ensuite une réponse de réussite ou d'échec au front-end. */
 app.delete("/api/stuff/:id", (req, res, next) => {
   Thing.deleteOne({ _id: req.params.id })
     .then(() => res.status(200).json({ message: "Objet supprimé !" }))
     .catch((error) => res.status(400).json({ error }));
 });
 
+/** Chapitre 10. Récupération de la liste de Things en vente.
+ * Nous utilisons la méthode find() dans notre modèle Mongoose afin de renvoyer un tableau contenant tous les Things dans notre base de données. À présent, si vous ajoutez un Thing , il doit s'afficher immédiatement sur votre page d'articles en vente. */
 app.get("/api/stuff", (req, res, next) => {
   Thing.find()
     .then((things) => res.status(200).json(things))
